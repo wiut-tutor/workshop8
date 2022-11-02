c2a4ea62de7a416d1ac9acf20f704c24d3c643e1
0000XXXX <46926302+wiut-tutor@users.noreply.github.com>
2022-11-02 13:53:30 +0500

    init: first

---
+++ workshop8.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3.4
+# Setup Python ----------------------------------------------- #
+import pygame, sys
+
+# Setup pygame/window ---------------------------------------- #
+mainClock = pygame.time.Clock()
+from pygame.locals import *
+
+pygame.init()
+pygame.display.set_caption('game base')
+screen = pygame.display.set_mode((500, 500), 0, 32)
+
+font = pygame.font.SysFont(None, 20)
+
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+
+click = False
+
+def main_menu():
+    while True:
+
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        pygame.display.update()
+        mainClock.tick(60)
+
+
+main_menu()
